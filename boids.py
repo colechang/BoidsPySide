43a33722826605f7558a426f2812156c72a8eead
colechang <colechang10@gmail.com>
2023-10-01 16:41:19 -0400

    outline for boids class

--- boids.py
+++ boids.py
@@ -1,8 +1,38 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsItem
+from PySide6.QtCore import Qt, QTimer
 
-app = QApplication(sys.argv)
-window = QWidget()
-window.show()
+# class BoidGraphicsItem(QGraphicsItem):
+# Implement rendering of a single boid here
 
-app.exec()
+
+class BoidSimulation(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Boid Simulation")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.scene = QGraphicsScene()
+        self.view = QGraphicsView(self.scene)
+        self.setCentralWidget(self.view)
+
+        # Create and add boid items to the scene
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_simulation)
+        self.timer.start(100)  # Update every 100 milliseconds
+
+    # def update_simulation(self):
+        # Update boid positions and redraw the scene
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = BoidSimulation()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()

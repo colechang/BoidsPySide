23db0b4ceaf126cc1b14ce4c3f1904ee8c414c67
colechang <colechang10@gmail.com>
2023-10-12 00:07:17 -0400

    dimension fixes

--- boids.py
+++ boids.py
@@ -2,27 +2,29 @@ import sys
 import random
 from PySide6.QtCore import Qt, QTimer
 from PySide6.QtGui import QPainter, QColor, QBrush
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QSlider, QLabel, QVBoxLayout, QWidget, QPushButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QSlider, QLabel, QVBoxLayout, QWidget
 import math
 
 # Define the parameters for the boids simulation
-NUM_BOIDS = 200
+NUM_BOIDS = 300
 BOID_SIZE = 5.0
 BOID_SPEED = 5
 BOID_COLOR = QColor(250, 240, 240)
 #Variables PySide will control 0.0-1.0 Tunable
-AVOID_FACTOR = 0.05   # Increase to encourage more avoidance
+AVOID_FACTOR = 0.005   # Increase to encourage more avoidance
 MATCHING_FACTOR = 0.05   # Increase to encourage more alignment
 CENTERING_FACTOR = 0.0005   # Increase to encourage more cohesion
 TURN_FACTOR = 0.2   # Reduce to make turns less aggressive
 MAX_SPEED = 3.0   # Reduce to limit maximum speed
 MIN_SPEED = 1.0
-VIEWING_DISTANCE = 3.0  # Adjust to control the neighborhood size
+VIEWING_DISTANCE = 8.0  # Adjust to control the neighborhood size
 PROTECTED_RANGE = 2.0   # Increase to encourage more collision avoidance
 MAXBIAS = 0.01
 BIAS_INCREMENT = 0.000004
 BIAS_GROUPS = ["LEFT","RIGHT"]
 BIAS_VAL = 0.001
+SCREEN_WIDTH = 0
+SCREEN_HEIGHT = 0
 
 # Class for an individual boid
 # right now comparing every boid to every boid n*n Time complexity
@@ -66,13 +68,13 @@ class Boid:
     def update(self):
         # change to collide with screen boundaries
         # FIXED: changed direction instead of location
-        if self.x < 200:
+        if self.x < 0:
             self.dx = self.dx + TURN_FACTOR
-        if self.x > window_width-200:
+        if self.x > window_width:
             self.dx = self.dx - TURN_FACTOR
-        if self.y < 100:
+        if self.y < 0:
             self.dy = self.dy + TURN_FACTOR
-        if self.y > window_height-100:
+        if self.y > window_height:
             self.dy = self.dy - TURN_FACTOR
             
         if (self.biasGroup =="LEFT"): 
@@ -156,7 +158,7 @@ class BoidsWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Boids Simulation")
-        self.setGeometry(0, 0, 800, 600)
+        self.setGeometry(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 
         # Create the central widget
         central_widget = QWidget()
@@ -168,9 +170,9 @@ class BoidsWindow(QMainWindow):
         # Create sliders and labels
         slider_layout = QHBoxLayout()
 
-        self.avoid_slider, avoid_label = self.create_slider("Avoid Factor", AVOID_FACTOR)
+        self.avoid_slider, avoid_label = self.create_sliderSeparation("Avoid Factor", AVOID_FACTOR)
         self.centering_slider, centering_label = self.create_sliderCentering("Centering Factor", CENTERING_FACTOR)
-        self.matching_slider, matching_label = self.create_slider("Matching Factor", MATCHING_FACTOR)
+        self.matching_slider, matching_label = self.create_sliderMatching("Matching Factor", MATCHING_FACTOR)
 
         slider_layout.addWidget(avoid_label)
         slider_layout.addWidget(self.avoid_slider)
@@ -192,10 +194,10 @@ class BoidsWindow(QMainWindow):
         layout.addWidget(boids_container)
         central_widget.setLayout(layout)
 
-    def create_slider(self, label_text, initial_value):
+    def create_sliderSeparation(self, label_text, initial_value):
         slider = QSlider(Qt.Horizontal)
         slider.setRange(1, 10)
-        slider.setValue(int(initial_value * 100))
+        slider.setValue(int(initial_value * 1000))
         slider.valueChanged.connect(self.slider_value_changed)
 
         label = QLabel(label_text)
@@ -210,21 +212,32 @@ class BoidsWindow(QMainWindow):
         label = QLabel(label_text)
 
         return slider, label
+    def create_sliderMatching(self, label_text, initial_value):
+        slider = QSlider(Qt.Horizontal)
+        slider.setRange(1, 10)
+        slider.setValue(int(initial_value * 100))
+        slider.valueChanged.connect(self.slider_value_changed)
 
+        label = QLabel(label_text)
+
+        return slider, label
     def slider_value_changed(self):
         # Update the variables based on the slider values
         global AVOID_FACTOR
         global CENTERING_FACTOR
         global MATCHING_FACTOR
-        AVOID_FACTOR = self.avoid_slider.value() / 100.0
-        CENTERING_FACTOR = self.centering_slider.value() / 100.0
-        MATCHING_FACTOR = self.matching_slider.value() / 10000.0
+        AVOID_FACTOR = self.avoid_slider.value() / 1000.0
+        CENTERING_FACTOR = self.centering_slider.value() / 10000.0
+        MATCHING_FACTOR = self.matching_slider.value() / 100.0
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    window_width = 800
-    window_height = 600
+    screen = app.primaryScreen()
+    size = screen.size()
+    window_width = size.width()
+    window_height = size.height()
+    SCREEN_HEIGHT = window_height
+    SCREEN_WIDTH = window_width
 
     window = BoidsWindow()
     window.show()
